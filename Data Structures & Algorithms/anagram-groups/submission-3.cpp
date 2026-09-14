9f59f11f551b3fc61ac23925c1fe11d0281134ce
btech10512 <btech10512.22@bitmesra.ac.in>
2026-09-14 15:56:38 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>>ans;
+        unordered_map<string,vector<string>>m;
+
+        for(int i=0;i<strs.size();i++) {
+            string cpy = strs[i];
+            sort(cpy.begin(),cpy.end());
+            m[cpy].push_back(strs[i]);
+        }
+        // for(string s:strs) {
+        //     string key = s;
+        //     sort(key.begin(),key.end());
+        //     m[key].push_back(s);
+        // }
+        for(auto &it:m) {
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
