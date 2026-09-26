9870079c6c93f87d9ccaf6a9c34fa98dee7d4a6e
btech10512 <btech10512.22@bitmesra.ac.in>
2026-09-26 14:12:16 +0530

    Add: reverse-a-linked-list - submission-0

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-0.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+
+    public:
+
+        ListNode* reverseList(ListNode* head) {
+
+                if(!head  || !head->next) return head;
+
+                        ListNode* curr = head;
+
+                                ListNode* prev = NULL;
+
+                                        ListNode* front = head->next;
+
+                                         
+
+                                                 while(curr) {
+
+                                                             curr->next = prev;
+
+                                                                         prev = curr;
+
+                                                                                     curr = front;
+
+                                                                                                 if(front) {
+
+                                                                                                                 front = front->next;
+
+                                                                                                                             }
+
+                                                                                                                                     }
+
+                                                                                                                                             return prev;
+
+                                                                                                                                                 }
+
+                                                                                                                                                 };
+
